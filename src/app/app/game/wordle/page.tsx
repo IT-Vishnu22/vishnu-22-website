adf7cbb1bb8d5870067090e57c6ea62493158a95
vinitian <140070514+vinitian@users.noreply.github.com>
2024-07-03 00:12:35 +0700

    add: wordle page

---
+++ src/app/app/game/wordle/page.tsx
@@ -0,0 +1,102 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { useState } from 'react';
+import wordList from './wordList.json'
+
+export default function WordlePage() {
+
+    const [ isGuessed, setIsGuessed ] = useState<boolean>(false);
+    const [ guess, setGuess ] = useState<string>('');
+
+    window.addEventListener("keyup", (e) => handleKeyUp(e.key));
+    
+    function handleKeyUp(key: string) {
+        if (key.length === 1 && key.match(/[A-z]/) && guess.length < 5) {
+            setGuess(guess + key);
+            console.log(guess);
+            return 0;
+        }
+    }
+
+    function handleClick(){
+
+        //to do: create popup component to use instead of alert()
+
+        if (guess.length === 5 && wordList.includes(guess.toLowerCase())) {
+            alert('you won!');
+            setIsGuessed(true);
+        }
+        else if (guess.length != 5) {
+            alert('Please enter a 5-letter word.');
+        }
+        else if (!wordList.includes(guess.toLowerCase())) {
+            alert('Invalid word');
+            // if you want to add a word e.g. chula, add it in wordList.json
+        }
+    }
+
+    return (
+        <div>
+
+            <div className="bg-blue-4 relative h-[91px] flex items-center justify-between">
+                <div className="pl-[35px]">---</div>
+                <h1 className="absolute right-16 left-16 text-center font-bold font-roboto-condensed text-[32px] text-white">
+                    Wordle
+                </h1>
+            </div>
+
+            <div className="flex flex-col items-center justify-center min-h-screen space-y-6">
+                <Guess
+                    answer={"hello"}
+                    guess={guess.toLowerCase()}
+                    isGuessed={isGuessed}
+                />
+                <Button
+                    className="w-32 h-16 font-bold border-4 border-blue-1 bg-blue-4 hover:bg-blue-1 rounded-xl shadow-[3px_4px_0px_#2A334E]"
+                    type="submit"
+                    onClick={handleClick}
+                >
+                    Submit
+                </Button>
+            </div>
+
+        </div>
+    );
+}
+
+function Guess({ answer, guess, isGuessed }) {
+
+    return (
+
+        <div className="flex justify-center">
+            <div className="grid grid-cols-5 gap-2">
+                {new Array(5).fill(0).map((_, i) => {
+                    const bgColor = !isGuessed
+                    ? 'bg-white'
+                    : guess[i] === answer[i]
+                    ? 'bg-green-300'
+                    : answer.includes(guess[i])
+                    ? 'bg-cream'
+                    : 'bg-white'
+
+                    if (isGuessed) {
+                        return(
+                            <div className= {`h-16 w-16 ${bgColor} items-center flex justify-center border-4 border-blue-4 rounded-xl font-bold uppercase text-blue-1 shadow-[3px_4px_0px_#427383]`}>
+                                {guess[i]}
+                            </div>
+                        ) 
+                    } else {
+                        return (
+                            <div className= {`h-16 w-16 bg-white items-center flex justify-center border-4 border-blue-4 rounded-xl font-bold uppercase text-blue-1 shadow-[3px_4px_0px_#427383]`}>
+                                {guess[i]}
+                            </div>
+                        );
+                    }
+                                       
+                })}
+            </div>
+        </div> 
+
+    );
+}
