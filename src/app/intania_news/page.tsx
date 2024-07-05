ec378ebed2d59109dd0bd7bb7ac075cf3fd0dfec
Thukdanai Thaothawin <arkaza2547@gmail.com>
2024-07-05 17:29:51 +0700

    page layout

---
+++ src/app/intania_news/page.tsx
@@ -0,0 +1,156 @@
+export default function Home() {
+  return (
+    <div className="font-roboto-condensed">
+      <div className="flex justify-center space-x-[10%] mt-10">
+        <div>vector</div> <div>intania news</div>
+      </div>
+
+
+      <div className=" mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
+        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+          <img
+            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            loading="lazy"
+            alt="Photo by Magicle"
+            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
+            Placeholder
+          </span>
+        </div>
+      </div>
+
+
+
+      <div className="grid gap-4 grid-cols-4 mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
+        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+          <img
+            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            loading="lazy"
+            alt="Photo by Magicle"
+            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
+            Placeholder
+          </span>
+        </div>
+        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+          <img
+            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            loading="lazy"
+            alt="Photo by Magicle"
+            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
+            Placeholder
+          </span>
+        </div>
+      </div>
+
+      <div className="grid gap-4 grid-cols-3 mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
+        <div>
+          <div className=""></div>
+          <div>
+            <h2 className="relative  mb-3 inline-block  text-black ml-5 text-lg break-words max-w-full">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, culpa esse. Maiores a dicta obcaecati molestiae facere tempora tempore accusamus, velit adipisci nemo ea autem laborum blanditiis repellat quam cumque.
+            </h2>
+          </div>
+        </div>
+
+        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+          <img
+            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            loading="lazy"
+            alt="Photo by Magicle"
+            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
+            Placeholder
+          </span>
+        </div>
+      </div>
+
+      <div className="grid gap-4 grid-cols-3 mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
+        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+          <img
+            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            loading="lazy"
+            alt="Photo by Magicle"
+            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
+            Placeholder
+          </span>
+        </div>
+        <div>
+          <h2 className="relative  mb-3 inline-block  text-black ml-5 text-lg break-words max-w-full">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, eveniet accusantium. Accusantium impedit, nisi fugiat recusandae voluptas perspiciatis provident quasi? Repudiandae, laudantium accusamus quos ducimus atque provident ad aliquid quidem.
+          </h2>
+        </div>
+      </div>
+
+      <div className=" mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
+        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+          <img
+            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            loading="lazy"
+            alt="Photo by Magicle"
+            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
+            Placeholder
+          </span>
+        </div>
+      </div>
+
+      
+
+      <div className="relative max-w-[40%] mx-auto mt-5">
+        <img
+          className="h-64 w-full object-cover rounded-md"
+          src="https://images.unsplash.com/photo-1680725779155-456faadefa26"
+          alt="Random image"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+
+/*
+<div className="relative max-w-[40%] mx-auto mt-10">
+  <img
+    className="h-64 w-full object-cover rounded-md"
+    src="https://images.unsplash.com/photo-1680725779155-456faadefa26"
+    alt="Random image"
+  ></img>
+  <div className="absolute inset-0 bg-gray-700 opacity-60 rounded-md"></div>
+  <div className="absolute inset-0 flex place-content-end">
+    <div className="content-end">
+      <h2 className="text-white text-3xl font-bold">
+        รีวิวชีวิตปี 1 วิศวฯจุฬา
+      </h2>
+      <h2 className="text-white text-3xl font-bold">placeholder2?</h2>
+    </div>
+  </div>
+</div>
+</div>
+*/
