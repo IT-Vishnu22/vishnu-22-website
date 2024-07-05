c008b1c48ce3140372d31626858d21aa306c7013
vinitian <140070514+vinitian@users.noreply.github.com>
2024-07-05 17:57:12 +0700

    add: close icon, info icon
    edit: fixed backspace bug in wordle, better tailwindcss, back icon

--- src/app/app/game/wordle/page.tsx
+++ src/app/app/game/wordle/page.tsx
@@ -1,35 +1,50 @@
 'use client';
 
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { useState } from 'react';
 import wordList from './wordList.json'
+import Link from "next/link";
+import { useEffect, useState } from 'react';
+import { Button } from "@/components/ui/button";
 import { BackIcon } from "@/components/BackIcon";
+import { InfoIcon } from "@/components/InfoIcon";
+import { CloseIcon } from '@/components/CloseIcon';
 
 export default function WordlePage() {
 
-    const [ isGuessed, setIsGuessed ] = useState<boolean>(false);
-    const [ guess, setGuess ] = useState<string>('');
-    const [ popUpMessage, setPopUpMessage ] = useState<string>('start');
-    window.addEventListener("keyup", (e) => handleKeyUp(e.key));
     const answer: string = "hello";
 
-    function handleKeyUp(key: string) {
-        if (key.length === 1 && key.match(/[A-z]/) && guess.length < 5) {
-            setGuess(guess + key);
-            console.log(guess);
+    const [isGuessed, setIsGuessed] = useState<boolean>(false);
+    const [guess, setGuess] = useState<string>('');
+    const [popUpMessage, setPopUpMessage] = useState<string>('rules');
+
+    useEffect(() => {
+        window.addEventListener("keyup", handleKeyUp);
+
+        return () => {
+            window.removeEventListener("keyup", handleKeyUp);
         }
-        // todo: fix the bug where pressing keys fast result in kinda frozen page
+    }, [guess]);
+
+
+    function handleKeyUp(e: { key: string; }) {
+        let key = e.key;
+        // (1) to do: fix bug where user is still able to delete 1 letter before not being able to
+        if (isGuessed === true) {
+            return 0;
+        }
+        else if (key.length === 1 && key.match(/[A-z]/) && guess.length < 5) {
+            setGuess(guess + key);
+        }
         else if (key === 'Backspace') {
-            setGuess(guess.slice(0,guess.length-1));
+            setGuess(guess.slice(0, guess.length - 1));
+        }
+        else if (key === 'Enter') {
+            handleSubmitClick();
         }
     }
-    
 
     function handleSubmitClick() {
-
         if (guess.length === 5 && guess.toLowerCase() === answer) {
-            setPopUpMessage('you won!');
+            setPopUpMessage('You won!');
             setIsGuessed(true);
         }
         else if (guess.length != 5) {
@@ -40,9 +55,12 @@ export default function WordlePage() {
             // if you want to add a word e.g. chula, add it in wordList.json
         }
         else {
-            setPopUpMessage('The word isn\'t quite your guess. Maybe discuss with your group?')
+            setPopUpMessage('The answer isn\'t quite your guess. Maybe discuss with your group?')
             setIsGuessed(true);
         }
+
+        // for debugging (1)
+        if (isGuessed){console.log('yes')}
     }
 
     function handlePopUpClick() {
@@ -50,37 +68,35 @@ export default function WordlePage() {
     }
 
     function handleRulePopUpClick() {
-
+        setPopUpMessage('rules');
     }
 
     return (
         <div className="font-roboto-condensed">
 
             <div className="bg-blue-4 relative h-[91px] flex items-center justify-between">
-                <div className="pl-[35px]"><Link href="/app/game"><BackIcon/></Link></div>
+                <div className="pl-[35px]"><Link href="/app/game"><BackIcon /></Link></div>
                 <h1 className="absolute right-16 left-16 text-center font-bold font-roboto-condensed text-[32px] text-white">
                     Wordle
                 </h1>
+                <div className="pr-[35px]"><Link href="" onClick={handleRulePopUpClick}><InfoIcon /></Link></div>
             </div>
 
+            {popUpMessage != '' &&
+                    <PopUp
+                        message={popUpMessage}
+                        onClick={handlePopUpClick}
+                    />}
+
             <div className="flex flex-col items-center pt-32 space-y-8">
-                {popUpMessage != '' &&
-                <PopUp
-                    message={popUpMessage}
-                    onClick={handlePopUpClick}
-                />}
-
-                <div className="flex">
-                    <Button type="button" className="w-20 h-10 font-bold border-4 border-blue-1 bg-blue-4 hover:bg-blue-1 rounded-xl shadow-[3px_4px_0px_#2A334E]" onClick={handleRulePopUpClick}>Rules</Button>
-                </div>
-
+            
                 <Guess
                     answer={"hello"}
                     guess={guess.toLowerCase()}
                     isGuessed={isGuessed}
                 />
                 <Button
-                    className="w-32 h-16 font-bold border-4 text-[20px] border-blue-1 bg-blue-4 hover:bg-blue-1 rounded-xl shadow-[3px_4px_0px_#2A334E]"
+                    className="w-32 h-16 font-bold border-2 text-[20px] border-blue-1 bg-blue-4 hover:bg-blue-1 rounded-xl shadow-[3px_4px_0px_#2A334E]"
                     type="submit"
                     onClick={handleSubmitClick}
                 >
@@ -92,62 +108,67 @@ export default function WordlePage() {
     );
 }
 
-function PopUp({ message, onClick }: { message: string, onClick: VoidFunction}) {
+function PopUp({ message, onClick }: { message: string, onClick: VoidFunction }) {
 
     const bgColor = (message === 'You won!')
-    ? 'bg-green-300'
-    : (message === 'Please enter a 5-letter word.') || (message === 'Please enter a valid word.')
-    ? 'bg-yellow-300'
-    : 'bg-gray-300'
+        ? 'bg-green-300'
+        : (message === 'Please enter a 5-letter word.') || (message === 'Please enter a valid word.')
+            ? 'bg-yellow-300'
+            : 'bg-gray-300'
 
-    return(
-        <div className={` ${bgColor} border-2 border-black rounded-xl p-4`}>
-             <Button
-                className="bg-transparent hover:bg-gray-400 text-black h-2 w-2 flex items-right"
-                onClick={onClick}
-             >
-                 x
-             </Button> 
-            {message != 'start'
-            ? <p>{message}</p>
-            : <p>'Welcome to Wordle! Before playing, here are some rules:<br/><br/>1. You can only guess a 5-letter word once.<br/>2.The faster a group has correctly guessed the word, the more score they get.<br/>3. You can discuss within your group what the correct word is.<br/><br/>Have fun!'</p>
-            }
+    return (
+        <div className="flex justify-center">
+            <div className={` ${bgColor} border-2 border-black rounded-xl p-4 m-4 absolute flex items-start`}>
+                <div className="pt-8">
+                    {message === 'rules'
+                        ? <p><b>Welcome to Wordle!</b> Before playing, here are some rules:<br /><br />1. You can only guess a 5-letter word once.<br />2. Group score is based on how many people correctly guess the word.<br />3. You can discuss within your group what the answer is.<br /><br />Have fun!</p>
+                        : <p className="text-center">{message}</p>
+                    }
+                </div>
+
+                <Button
+                    className="bg-transparent hover:bg-[rgba(0,0,0,0.1)] text-black absolute top-0 right-0 m-2"
+                    onClick={onClick}
+                >
+                    <CloseIcon />
+                </Button>
+
+            </div>
         </div>
     );
 }
 
 function Guess({ answer, guess, isGuessed }: { answer: string, guess: string, isGuessed: boolean }) {
-    // to do: collect guess for each person so that it does not change even after reload
     return (
 
         <div className="flex justify-center">
             <div className="grid grid-cols-5 gap-2">
                 {new Array(5).fill(0).map((_, i) => {
                     const bgColor = !isGuessed
-                    ? 'bg-white'
-                    : guess[i] === answer[i]
-                    ? 'bg-green-300'
-                    : answer.includes(guess[i])
-                    ? 'bg-cream'
-                    : 'bg-white'
+                        ? 'bg-white'
+                        : guess[i] === answer[i]
+                            ? 'bg-green-300'
+                            : answer.includes(guess[i])
+                                ? 'bg-cream'
+                                : 'bg-white'
 
                     if (isGuessed) {
-                        return(
-                            <div className= {`h-16 w-16 ${bgColor} items-center flex justify-center border-4 border-blue-4 rounded-xl font-bold uppercase text-blue-1 shadow-[3px_4px_0px_#427383]`}>
+                        return (
+                            <div className={`h-16 w-16 ${bgColor} items-center flex justify-center border-2 border-blue-4 rounded-xl font-bold uppercase text-blue-1 shadow-[3px_4px_0px_#427383]`}>
                                 {guess[i]}
                             </div>
-                        ) 
+                        )
                     } else {
                         return (
-                            <div className= {`h-16 w-16 bg-white items-center flex justify-center border-4 border-blue-4 rounded-xl font-bold uppercase text-blue-1 shadow-[3px_4px_0px_#427383]`}>
+                            <div className={`h-16 w-16 bg-white items-center flex justify-center border-2 border-blue-4 rounded-xl font-bold uppercase text-blue-1 shadow-[3px_4px_0px_#427383]`}>
                                 {guess[i]}
                             </div>
                         );
                     }
-                                       
+
                 })}
             </div>
-        </div> 
+        </div>
 
     );
 }
--- src/components/BackIcon.tsx
+++ src/components/BackIcon.tsx
@@ -2,14 +2,16 @@ export const BackIcon = () => {
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="25"
-        height="22"
-        viewBox="0 0 25 22"
         fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="size-6"
       >
         <path
-          d="M7.8125 8.20714H17.1875C19.7719 8.20714 21.875 10.3725 21.875 13.0333C21.875 15.6941 19.7719 17.8594 17.1875 17.8594H12.5V21.0768H17.1875C21.4953 21.0768 25 17.4685 25 13.0333C25 8.59805 21.4953 4.98971 17.1875 4.98971H7.8125V0.163574L0 6.59842L7.8125 13.0333V8.20714Z"
-          fill="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
         />
       </svg>
     );
