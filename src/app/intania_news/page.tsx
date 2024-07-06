7fb7c64961b50473c576de798f06e72829152f4e
Thukdanai Thaothawin <arkaza2547@gmail.com>
2024-07-06 21:19:21 +0700

    all component provide need resource

--- src/app/intania_news/page.tsx
+++ src/app/intania_news/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
-    <div className="font-roboto-condensed">
-      <div className="flex justify-center space-x-[10%] mt-10">
+           
+    <>
+      <div className=" flex justify-center space-x-[10%] mt-10">
         <div>vector</div> <div>intania news</div>
       </div>
 
-
-      <div className=" mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
-        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+      <div className=" mx-auto gap-6   max-w-[80%] mt-5">
+        <div className="group relative flex  items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-64">
           <img
-            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            src="https://placehold.co/1080x720"
             loading="lazy"
             alt="Photo by Magicle"
             className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
@@ -17,58 +17,23 @@ export default function Home() {
 
           <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
-            Placeholder
+          <span className="relative ml-4 mb-3 inline-block text-xs text-white ml-5 text-xs">
+            รีวิวชีวิตปี 1 วิศวฯจุฬา
           </span>
         </div>
       </div>
 
-
-
-      <div className="grid gap-4 grid-cols-4 mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
-        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
-          <img
-            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
-            loading="lazy"
-            alt="Photo by Magicle"
-            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-          />
-
-          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
-
-          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
-            Placeholder
-          </span>
-        </div>
-        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
-          <img
-            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
-            loading="lazy"
-            alt="Photo by Magicle"
-            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-          />
-
-          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
-
-          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
-            Placeholder
-          </span>
-        </div>
-      </div>
-
-      <div className="grid gap-4 grid-cols-3 mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
+      <div className="grid gap-4 grid-cols-3 mx-auto gap-6  max-w-[80%] mt-5">
         <div>
-          <div className=""></div>
           <div>
-            <h2 className="relative  mb-3 inline-block  text-black ml-5 text-lg break-words max-w-full">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, culpa esse. Maiores a dicta obcaecati molestiae facere tempora tempore accusamus, velit adipisci nemo ea autem laborum blanditiis repellat quam cumque.
+            <h2 className="relative  mb-3 inline-block  text-black ml-5 text-xs break-words max-w-full">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
             </h2>
           </div>
         </div>
-
-        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+        <div className="group relative flex  items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-64">
           <img
-            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            src="https://placehold.co/1080x720"
             loading="lazy"
             alt="Photo by Magicle"
             className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
@@ -76,16 +41,16 @@ export default function Home() {
 
           <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
-            Placeholder
+          <span className="relative ml-4 mb-3 inline-block text-xs text-white ml-5 text-xs">
+          SOTUS ตัดทิ้งไป
           </span>
         </div>
-      </div>
+      </div>   
 
-      <div className="grid gap-4 grid-cols-3 mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
-        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+      <div className="grid gap-4 grid-cols-3 mx-auto gap-6  max-w-[80%] mt-5">
+        <div className="group relative flex  items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-64">
           <img
-            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            src="https://placehold.co/1080x720"
             loading="lazy"
             alt="Photo by Magicle"
             className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
@@ -93,21 +58,22 @@ export default function Home() {
 
           <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
-            Placeholder
+          <span className="relative ml-4 mb-3 inline-block text-xs text-white ml-5 text-xs">
+          วิศวฯกับการเรียน
           </span>
         </div>
         <div>
-          <h2 className="relative  mb-3 inline-block  text-black ml-5 text-lg break-words max-w-full">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, eveniet accusantium. Accusantium impedit, nisi fugiat recusandae voluptas perspiciatis provident quasi? Repudiandae, laudantium accusamus quos ducimus atque provident ad aliquid quidem.
+          <h2 className="relative  mb-3 inline-block  text-black ml-5 text-xs break-words max-w-full">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+            
           </h2>
         </div>
       </div>
 
-      <div className=" mx-auto gap-6 xl:gap-8 max-w-[40%] mt-5">
-        <div className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-80">
+      <div className="grid gap-4 grid-cols-4 mx-auto gap-6  max-w-[80%] mt-5">
+        <div className="group relative flex  items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-64">
           <img
-            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+            src="https://placehold.co/1080x720"
             loading="lazy"
             alt="Photo by Magicle"
             className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
@@ -115,25 +81,53 @@ export default function Home() {
 
           <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-          <span className="relative ml-4 mb-3 inline-block text-sm text-white ml-5 text-lg">
-            Placeholder
+          <span className="relative ml-4 mb-3 inline-block text-xs text-white ml-5 text-xs">
+           ฟุตบอลประเพณี
+          </span>
+        </div>
+        <div className="group relative flex  items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-64">
+          <img
+            src="https://placehold.co/1080x720"
+            loading="lazy"
+            alt="Photo by Magicle"
+            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+          <span className="relative ml-4 mb-3 inline-block text-xs text-white ml-5 text-xs">
+          ไอเทมวิศวฯ 
           </span>
         </div>
       </div>
 
-      
+      <div className=" mx-auto gap-6  max-w-[80%] mt-5">
+        <div className="group relative flex  items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg col-span-2 h-64">
+          <img
+            src="https://placehold.co/1080x720"
+            loading="lazy"
+            alt="Photo by Magicle"
+            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
 
-      <div className="relative max-w-[40%] mx-auto mt-5">
+          <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+
+          <span className="relative ml-4 mb-3 inline-block text-xs text-white ml-5 text-xs">
+          บิดาแห่งคณะวิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย
+          </span>
+        </div>
+      </div>
+      
+      <div className="relative max-w-[80%] mx-auto mt-5">
         <img
           className="h-64 w-full object-cover rounded-md"
-          src="https://images.unsplash.com/photo-1680725779155-456faadefa26"
+          src="https://placehold.co/1080x720"
           alt="Random image"
         ></img>
       </div>
-    </div>
+    </>
   );
 }
-
 
 /*
 <div className="relative max-w-[40%] mx-auto mt-10">
