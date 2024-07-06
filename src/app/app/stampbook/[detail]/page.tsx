774181132a1615007aff745f6b0e4a649c941c41
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-07-06 19:48:35 +0700

    feat: add simple detail page

--- src/app/app/stampbook/[detail]/page.tsx
+++ src/app/app/stampbook/[detail]/page.tsx
@@ -1,17 +1,46 @@
+import Image from "next/image";
 import InputForm from "./InputForm";
+import Link from "next/link";
+import Question from "../question.json";
+import type { QuestionData } from "../page";
 
-export default function DetailPage() {
+function getQuestionDetail(name: string) {
+  return Question.QuestFirstDate.Locations.filter(
+    (item) => item.NameEN === name.replaceAll("-", " "),
+  )[0];
+}
+
+export default function DetailPage({ params }: { params: { detail: string } }) {
+  const questionDetails: QuestionData = getQuestionDetail(params.detail);
+
   return (
-    <main>
-      <h1>Heading</h1>
-      <img />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ea
-        voluptates ratione eveniet aliquam facere reprehenderit ipsa voluptas
-        optio tempora fugit pariatur ipsam nihil, facilis tenetur sed, excepturi
-        corrupti dolore?
+    <main className="w-full bg-green-1">
+      <header className="grid grid-cols-4 items-center bg-blue-4 p-8 text-[32px] font-semibold text-white">
+        <Link href="/app/stampbook" className="text-blue-3">
+          <Image
+            src="/stampbookImages/back.svg"
+            alt="back button"
+            width={25}
+            height={25}
+          />
+        </Link>
+        <h1 className="col-span-2 text-center">{questionDetails.NameTH}</h1>
+      </header>
+      <div className="relative h-52 w-full">
+        <Image
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+          src={`/stampbookImages/${questionDetails.NameTH}.jpg`}
+          alt={`${questionDetails.NameEN} location image`}
+          fill={true}
+        />
+      </div>
+      <p className="px-5 py-9 text-base font-medium text-blue-1">
+        {questionDetails.Fact}
       </p>
-      <section>
+      <section className="rounded-t-[50px] bg-[#FFFBF4]">
         <InputForm />
       </section>
     </main>
