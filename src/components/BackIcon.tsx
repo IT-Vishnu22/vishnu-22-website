389880288e941f895f9650d69f4b9bc5c98e68d1
Yiwahpsp <Patsaporn.Kaewkong@gmail.com>
2024-07-15 04:26:16 +0700

    fix: error strokeLinejoin

--- src/components/BackIcon.tsx
+++ src/components/BackIcon.tsx
@@ -10,7 +10,7 @@ export const BackIcon = () => {
       >
         <path
           strokeLinecap="round"
-          stroke-linejoin="round"
+          strokeLinejoin="round"
           d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
         />
       </svg>
