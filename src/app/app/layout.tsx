beb6d1f6929aa1657bb10cc18fda9a703ef60669
natapol2547 <robloxplay41@gmail.com>
2024-07-15 19:26:04 +0700

    remove: Extra navbar

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import Navbar from "@/components/Navbar";
-import { UserContext } from "@/lib/context";
-import { useUserData } from "@/lib/hooks";
 
 export default function Applayout({ children }: { children: React.ReactNode }) {
-  const userData = useUserData();
-  return (
-    <section>
-      <UserContext.Provider value={userData}>
-        {children} <Navbar />
-      </UserContext.Provider>
-    </section>
-  );
+    return (
+        <>
+            {children}
+            {/* <Navbar /> */}
+        </>
+    );
 }
