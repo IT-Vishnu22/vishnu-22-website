af506d7a6ae14e7edd999a6b0cd1a2b49dd3bef8
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-07-15 19:53:11 +0700

    refactor: relocate file to fix responsive problem

---
+++ src/app/app/(onlyMobile)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+export default function Applayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <main className="relative h-full w-full md:mx-auto md:max-w-[390px]">
+                {children}
+                <Navbar />
+            </main>
+        </>
+    );
+}
