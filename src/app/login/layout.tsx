dfd594b140bc5f79ee6de4d4243f74252d5cd4cf
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-07-15 20:04:17 +0700

    refactor: add layout for login & leaderboard to fix responsive layout

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -3,10 +3,9 @@ import Navbar from "@/components/Navbar";
 export default function Applayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <div className="relative h-full w-full md:mx-auto md:max-w-[390px]">
+            <main className="relative h-full w-full md:mx-auto md:max-w-[390px]">
                 {children}
-                <Navbar />
-            </div>
+            </main>
         </>
     );
 }
