e169efd93f103c01dda2d20773478c996a070cd8
Warich Kanchai <ohmwarich@gmail.com>
2024-07-15 23:41:09 +0700

    blank.svg

--- src/app/intania_news/intania_playlist/page.tsx
+++ src/app/intania_news/intania_playlist/page.tsx
@@ -8,7 +8,7 @@ import { FaPlay } from "react-icons/fa";
 
 export default function IntaniaPlaylist() {
   return (
-    <div className="relative h-full w-full bg-[linear-gradient(to_top,rgba(163,64,64,0.5),rgba(240,225,219,0.5)),url('/')] px-5 py-5">
+    <div className="relative h-full w-full bg-[linear-gradient(to_top,rgba(163,64,64,0.5),rgba(240,225,219,0.5)),url('/intaniastorypics/blank.svg')] px-5 py-5">
       <div className="absolute inset-x-0 bottom-0 h-[130px] w-full bg-[url('/intaniastorypics/dot_graphic.svg')] bg-contain bg-repeat-x"></div>
       <div className="relative mt-5 flex items-center justify-center">
         <Link href="/intania_news">
@@ -23,7 +23,7 @@ export default function IntaniaPlaylist() {
         </h1>
       </div>
 
-      <div className="mt-5 flex items-center justify-center space-x-4">
+      <div className="mt-6 flex items-center justify-center space-x-4">
         <div className="relative h-52 w-52 overflow-hidden bg-gray-100 shadow-lg">
           <img
             src="/intaniastorypics/IMG_1175.svg"
