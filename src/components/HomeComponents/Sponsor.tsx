c1da0bbc5fba62fdf42c9d7d8891cc52998a690b
Yiwahpsp <Patsaporn.Kaewkong@gmail.com>
2024-07-18 09:19:00 +0700

    fix: list of sponsor

--- src/components/HomeComponents/Sponsor.tsx
+++ src/components/HomeComponents/Sponsor.tsx
@@ -6,21 +6,22 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel";
 import useEmblaCarousel from "embla-carousel-react";
-import ImageMock from "@/assets/images/200x200.png";
 import Autoplay from "embla-carousel-autoplay";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 import TOA from "@/assets/images/sponsorImages/toa.png";
 import Shabu from "@/assets/images/sponsorImages/shabu.png";
-import Entermonth from "@/assets/images/sponsorImages/entermonth.jpg";
 import CQK from "@/assets/images/sponsorImages/cqk.png";
 import AutumnScoop from "@/assets/images/sponsorImages/autumnscoop.jpg";
-import Acadex from "@/assets/images/sponsorImages/acadex.png";
 
 
 export function SponsorCarousel() {
+
+    const sponsor: StaticImageData[] = [TOA, Shabu, AutumnScoop, CQK];
+        
+
     return (
-        <div className="overflow-hidden bg-white">
+        <div className="overflow-hidden bg-white flex justify-center">
             <Carousel
                 opts={{
                     align: "start",
@@ -33,36 +34,17 @@ export function SponsorCarousel() {
                 ]}
                 className="p-0 m-0">
                 <CarouselContent>
-                    <CarouselItem className={`basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6`}>
-                        <div className={`w-full h-full bg-white flex justify-center items-center px-2`}>
-                            <Image alt="sponser image" src={TOA} width={0} height={0} className="w-[90%] sm:w-[70%] h-auto object-contain" />
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className={`basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6`}>
-                        <div className={`w-full h-full bg-white flex justify-center items-center px-2`}>
-                            <Image alt="sponser image" src={AutumnScoop} width={0} height={0} className="w-[90%] sm:w-[70%] h-auto object-contain" />
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className={`basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6`}>
-                        <div className={`w-full h-full bg-white flex justify-center items-center px-2`}>
-                            <Image alt="sponser image" src={CQK} width={0} height={0} className="w-[90%] sm:w-[70%] h-auto object-contain" />
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className={`basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6`}>
-                        <div className={`w-full h-full bg-white flex justify-center items-center px-2`}>
-                            <Image alt="sponser image" src={Entermonth} width={0} height={0} className="w-[90%] sm:w-[70%] h-auto object-contain" />
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className={`basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6`}>
-                        <div className={`w-full h-full bg-white flex justify-center items-center px-2`}>
-                            <Image alt="sponser image" src={Acadex} width={0} height={0} className="w-[90%] sm:w-[70%] h-auto object-contain" />
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className={`basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6`}>
-                        <div className={`w-full h-full flex justify-center items-center bg-white px-2`}>
-                            <Image alt="sponser image" src={Shabu} width={0} height={0} className="w-[90%] sm:w-[70%] h-auto object-contain" />
-                        </div>
-                    </CarouselItem>
+                    {
+                        sponsor.map((item: any, index: number) => {
+                            return (
+                                <CarouselItem className={`basis-1/3 md:basis-1/4`} key={index}>
+                                    <div className={`w-full h-full bg-white flex justify-center items-center px-2`}>
+                                        <Image alt="sponser image" src={item} width={0} height={0} className="w-[80%] sm:w-[70%] md:w-[60%] max-w-[250px] h-auto object-contain" />
+                                    </div>
+                                </CarouselItem>
+                            );
+                        })
+                    }
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
