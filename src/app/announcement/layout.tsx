60107482c0df388bc8c778905d2502fc441e6ed8
Mingpassawat <66895039+Mingpassawat@users.noreply.github.com>
2024-07-18 14:42:00 +0700

    Modify: Announcement to have mobile layout only.

--- src/app/announcement/layout.tsx
+++ src/app/announcement/layout.tsx
@@ -5,12 +5,14 @@ import { UserContext } from "@/lib/context";
 import { useUserData } from "@/lib/hooks";
 
 export default function Applayout({ children }: { children: React.ReactNode }) {
-  const userData = useUserData();
-  //const userData = {user: null, "username": "Passawatna A.", group:"L"};
+  //const userData = useUserData();
+  const userData = {user: null, "username": "Passawatna A.", group:"L"};
   return (
     <section>
       <UserContext.Provider value={userData}>
-        {children}
+        <div className="relative h-full w-full md:mx-auto md:max-w-[390px]">
+          {children}
+        </div>
       </UserContext.Provider>
     </section>
   );
