944e402f37c3bed78301cdbcf074731a0d88a514
Mingpassawat <66895039+Mingpassawat@users.noreply.github.com>
2024-07-18 11:32:05 +0700

    Fix: a bug

--- src/app/app/(onlyMobile)/game/wordle/page.tsx
+++ src/app/app/(onlyMobile)/game/wordle/page.tsx
@@ -35,6 +35,8 @@ export default function WordlePage() {
             }
             if (played || !answer) {
                 setIsGuessed(true);
+            } else {
+                setIsGuessed(false)
             }
             updatePlayed();
         }, [played, answer])
