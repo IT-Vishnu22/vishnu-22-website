a40b2f450ecfc4aa265dc7dcde41b8978747cd0d
Iyako <pannawish.boo@mail.kmutt.ac.th>
2024-07-18 22:51:30 +0700

    add: Full Popcat Backend (Except Group Score)
    All my popcat work

--- src/app/app/(onlyMobile)/game/popcat/page.tsx
+++ src/app/app/(onlyMobile)/game/popcat/page.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Image from "next/image";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import ScoreDisplay from "@/components/ScoreDisplay";
@@ -10,31 +10,52 @@ import {
     ResizableHandle,
     ResizablePanel,
     ResizablePanelGroup,
-  } from "@/components/ui/resizable"
+} from "@/components/ui/resizable";
+import { UserContext } from "@/lib/contexts/user";
+import { addClick, onLoad } from "@/lib/popgear/actions";
+import { set } from "firebase/database";
+import { Console } from "console";
 
 export default function PopcatPage() {
-    const [ picUrl, setPicUrl ] = useState('/pic1.jpg');
-    const [ count, setCount ] = useState(0);
-    const [ imageSize, setImageSize ] = useState('65%');
+    const [picUrl, setPicUrl] = useState("/pic1.jpg");
+    const [count, setCount] = useState(0);
+    const [imageSize, setImageSize] = useState("65%");
+    const user = useContext(UserContext);
+ 
+
+
+    // useEffect(() => {
+    //     if (user.data) {
+    //         onLoad(user.data.studentId, user.data.group).then((e) => {
+    //             setCount(e);
+    //         });
+    //     }
+    // }, [user]);
 
     function touchStart() {
-        setPicUrl('/pic2.jpg');
-        setCount(count+1);
-        setImageSize('80%');
+        setPicUrl("/pic2.jpg");
+        setCount(count + 1);
+        setImageSize("80%");
+        addClick("", "", count);
+        
+        // if (user.data) {
+        //     addClick(user.data.studentId, user.data.group, count);
+            
+        // }
     }
 
-    function touchEnd(e: { preventDefault: () => void; }) {
-        setPicUrl('/pic1.jpg');
-        e.preventDefault()
-        setImageSize('77%');
+    function touchEnd(e: { preventDefault: () => void }) {
+        setPicUrl("/pic1.jpg");
+        e.preventDefault();
+        setImageSize("77%");
         setTimeout(() => {
-            setImageSize('74%');
+            setImageSize("74%");
             setTimeout(() => {
-                setImageSize('71%');
+                setImageSize("71%");
                 setTimeout(() => {
-                    setImageSize('68%');
+                    setImageSize("68%");
                     setTimeout(() => {
-                        setImageSize('65%');
+                        setImageSize("65%");
                     }, 25);
                 }, 25);
             }, 25);
@@ -42,41 +63,56 @@ export default function PopcatPage() {
     }
 
     return (
-        <div className="flex flex-col h-full">
-            <div className="flex-none bg-blue-4 relative h-[91px] flex items-center justify-between z-[3]">
-                <div className="pl-[35px]"><Link href="/app/game"><BackIcon/></Link></div>
-                <h1 className="absolute right-16 left-16 text-center font-bold font-roboto-condensed text-[32px] text-white">
+        <div className="flex h-full flex-col">
+            <div className="relative z-[3] flex h-[91px] flex-none items-center justify-between bg-blue-4">
+                <div className="pl-[35px]">
+                    <Link href="/app/game">
+                        <BackIcon />
+                    </Link>
+                </div>
+                <h1 className="absolute left-16 right-16 text-center font-roboto-condensed text-[32px] font-bold text-white">
                     Pop Cat
                 </h1>
             </div>
             <div className="relative w-full grow">
-                <h1 className="absolute right-16 left-16 pt-5 text-center font-bold font-roboto-condensed text-[60px]">
+                <h1 className="absolute left-16 right-16 pt-5 text-center font-roboto-condensed text-[60px] font-bold">
                     {count}
                 </h1>
                 <div className="absolute inset-0 flex flex-col items-center">
                     {/* {picUrl} */}
-                    <Image id="popEle" 
-                        width={0} 
-                        height={0} 
-                        style={{ width: `${imageSize}`, height: 'auto', marginInline: 'auto', marginTop: '50%' }}
-                        src='/vercel.svg' 
-                        alt="pop element" />
+                    <Image
+                        id="popEle"
+                        width={0}
+                        height={0}
+                        style={{
+                            width: `${imageSize}`,
+                            height: "auto",
+                            marginInline: "auto",
+                            marginTop: "50%",
+                        }}
+                        src="/vercel.svg"
+                        alt="pop element"
+                    />
                 </div>
             </div>
-            <ResizablePanelGroup 
+            <ResizablePanelGroup
                 className="absolute bottom-[85px]"
-                direction="vertical">
+                direction="vertical"
+            >
                 <ResizablePanel
-                    className=" p-[81px]"
-                    onTouchStart={touchStart} onTouchEnd={touchEnd}
-                    onMouseDown={touchStart} onMouseUp={touchEnd}/>
-                <ResizableHandle className="bg-transparent p-[15px] mb-[-20px] z-[3]"/>
+                    className="p-[81px]"
+                    onTouchStart={touchStart}
+                    onTouchEnd={touchEnd}
+                    onMouseDown={touchStart}
+                    onMouseUp={touchEnd}
+                />
+                <ResizableHandle className="z-[3] mb-[-20px] bg-transparent p-[15px]" />
                 <ResizablePanel>
-                    <ScrollArea className="flex items-stretch font-roboto-condensed rounded-t-[50px] bg-white w-full px-[10px] pt-[20px] h-full">
-                        <ScoreDisplay start={1}/>
+                    <ScrollArea className="flex h-full w-full items-stretch rounded-t-[50px] bg-white px-[10px] pt-[20px] font-roboto-condensed">
+                        <ScoreDisplay start={1} />
                     </ScrollArea>
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
-    )
+    );
 }
---
+++ src/lib/popgear/actions.ts
@@ -0,0 +1,55 @@
+'use client';
+
+import { set } from "firebase/database";
+import { firestore } from "../firebase";
+//import { firebase } from "../firebase"
+import { doc, getDoc, updateDoc, writeBatch, FieldValue, increment, setDoc} from "firebase/firestore"
+
+let debounceTimer: NodeJS.Timeout;
+
+export async function addClick(studentId: string | undefined, group: string | undefined, count: number) {
+    
+    clearTimeout(debounceTimer);
+
+    debounceTimer = setTimeout(async () => {
+        // if (!studentId || !group) {
+        //     console.error("DNE");
+        //     return;
+        // }
+    
+        // const docRef = doc(firestore, "user", studentId);
+    
+        // updateDoc(docRef, {
+        //     clicks: count
+        // })
+        console.log(count)
+    }, 500);
+
+    
+}
+
+
+export async function onLoad(studentId: string | undefined, group: string | undefined) {
+    console.log("played: ", studentId, group);
+
+    if (!group || !studentId) {
+        return 0;
+    }
+
+    const docRef = doc(firestore, "users", studentId);
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists()) {
+        console.error("DNE");
+        return 0;
+    }
+
+    if (docSnap.data().clicks == null){
+        setDoc(docRef, {clicks: 0}, {merge: true})
+        return 0;
+    }
+    else
+    {
+        return docSnap.data().clicks;
+    }    
+}
