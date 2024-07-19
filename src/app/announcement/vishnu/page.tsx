c97ca24c187c96e0bb0623d1ec078f3de62e92e8
Mingpassawat <66895039+Mingpassawat@users.noreply.github.com>
2024-07-19 15:49:54 +0700

    Add: condition for vishunu announcement page

--- src/app/announcement/vishnu/page.tsx
+++ src/app/announcement/vishnu/page.tsx
@@ -216,12 +216,23 @@ const AnnouncementDetail: React.FC<AnnouncementDetailProps> = ({ houseName, regi
   const img2 = `/announcement/vishnu/second/${meetingPoint}.png`;
   return (
     <>
-      <div className="my-4 text-center font-athiti text-white">
-        <h1 className="text-[32px] font-bold leading-[52px]">หน่วย{groupName}</h1>
-        <p className="text-base font-medium">{meetingPoint}</p>
-      </div>
-      <MapCarousel img1={img1} img2={img2}/>
+      {
+        group ? 
+          <>
+            <div className="my-4 text-center font-athiti text-white">
+              <h1 className="text-[32px] font-bold leading-[52px]">หน่วย{groupName}</h1>
+              <p className="text-base font-medium">{meetingPoint}</p>
+            </div>
+            <MapCarousel img1={img1} img2={img2}/> 
+          </>
+          :
+          <div className="my-4 text-center font-athiti text-white ">
+            <p className="text-[24px]">404:ไม่พบหน่วยของคุณ.</p><br />
+            <p>หากคุณคือรุ่น 108 กรุณาล็อคอินหรือติดต่อ IT</p>
+          </div>
+      }
     </>
+    
   );
 };
 interface AnnouncementDetailProps {
