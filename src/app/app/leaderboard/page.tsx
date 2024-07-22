635be7cca5af9d1f474f953b7bca6540f8371ca7
neennera <neennera@gmail.com>
2024-07-22 21:00:16 +0700

    style: change wording 'Operation Aqua Spy'

--- src/app/app/leaderboard/page.tsx
+++ src/app/app/leaderboard/page.tsx
@@ -71,7 +71,7 @@ export default function Leaderboard() {
             </div>
 
             {/* back icon & title */}
-            <Title />
+            <Title type={showType} />
 
             {/* other sai score */}
             <ScoreDisplay
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
 import { BackIcon } from "./BackIcon";
 
-export default function Title() {
-  return (
-    <>
-      <div className="w-full mr-[10px] pt-[35px] absolute items-center top-0 flex flex-row justify-start px-[10px] sm:px-[30px]">
-        <Link href="/">
-          <BackIcon />
-        </Link>
+export default function Title(props: { type: string }) {
+    return (
+        <>
+            <div className="absolute top-0 mr-[10px] flex w-full flex-row items-center justify-start px-[10px] pt-[35px] sm:px-[30px]">
+                <Link href="/">
+                    <BackIcon />
+                </Link>
 
-        <p className="text-lg sm:text-3xl relative text-white self-center flex-1 text-center font-semibold">
-          Leaderboard
-        </p>
-      </div>
-    </>
-  );
+                {props.type === "game" ? (
+                    <p className="relative flex-1 self-center text-center text-lg font-semibold text-white sm:text-3xl">
+                        Leaderboard
+                    </p>
+                ) : (
+                    <p className="relative flex-1 self-center text-center text-lg font-semibold text-white sm:text-3xl">
+                        Operation Aqua Spy
+                    </p>
+                )}
+            </div>
+        </>
+    );
 }
