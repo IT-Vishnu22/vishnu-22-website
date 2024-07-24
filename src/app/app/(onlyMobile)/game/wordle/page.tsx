f36bcc9b070c7ad7d61dd93bcb572cc7e45a90ff
Thames <chiratt.thames@gmail.com>
2024-07-24 10:45:40 +0700

    Update page.tsx

--- src/app/app/(onlyMobile)/game/wordle/page.tsx
+++ src/app/app/(onlyMobile)/game/wordle/page.tsx
@@ -178,7 +178,7 @@ export default function WordlePage() {
                             </>
                         )
                     ) : (
-                        <p>Closed. Game open: 13.00 - 20.00</p>
+                        <p>Closed. Game open: 9.00 - 20.00</p>
                     )}
                 </div>
             </div>
