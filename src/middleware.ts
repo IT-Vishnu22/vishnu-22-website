3b213aa834cc5b53ee0651c45c367fcbf2a9ba36
Thames <chiratt.thames@gmail.com>
2024-07-25 21:47:23 +0700

    Closed game + stampbook

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,18 +5,21 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     // Extract the query parameters from the request URL
     if (request.nextUrl.pathname == "/") {
-      const queryParams = request.nextUrl.search;
-      // Construct the new URL with the query parameters
-      const newUrl = new URL(`/app/home${queryParams}`, request.nextUrl.origin);
+        const queryParams = request.nextUrl.search;
+        // Construct the new URL with the query parameters
+        const newUrl = new URL(
+            `/app/home${queryParams}`,
+            request.nextUrl.origin,
+        );
 
-      // Redirect to the new URL
-      return NextResponse.redirect(newUrl);
-  }
+        // Redirect to the new URL
+        return NextResponse.redirect(newUrl);
+    }
 
     if (
-        request.nextUrl.pathname.startsWith("/app/game/popgear") 
+        request.nextUrl.pathname.startsWith("/app/game") ||
         // || request.nextUrl.pathname.startsWith("/app/leaderboard")
-        // || request.nextUrl.pathname.startsWith("/announcement/vishnu")
+        request.nextUrl.pathname.startsWith("/app/stampbook")
     ) {
         return NextResponse.rewrite(new URL("/app/home", request.url));
     }
@@ -26,9 +29,9 @@ export function middleware(request: NextRequest) {
 export const config = {
     matcher: [
         "/",
-        // "/app/game/:path*",
+        "/app/game/:path*",
         // "/app/leaderboard/:path*",
         // "/announcement/vishnu",
-        "/app/game/popgear/:path*",
+        "/app/stampbook:path*",
     ],
 };
